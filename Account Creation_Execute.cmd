94db4066a191645a41fd7fdc81ca796b7538bd84
Praveen4sf <35450837+Praveen4sf@users.noreply.github.com>
2019-04-12 13:58:11 +0530

    Add files via upload

---
+++ Account Creation_Execute.cmd
@@ -0,0 +1,7 @@
+If /I "%Processor_Architecture%" NEQ "x86" (
+%SystemRoot%\SysWoW64\WindowsPowerShell\v1.0\powershell.exe /C %0
+goto :eof
+)
+pushd %~dp0
+cscript "C:\Program Files (x86)\Inflectra\Rapise\Engine\SeSExecutor.js" "C:\Users\sysadmin\Desktop\Praveen\Account Creation\Account Creation.sstest" "-eval:g_testSetParams={userName:'', password:''};"
+popd
